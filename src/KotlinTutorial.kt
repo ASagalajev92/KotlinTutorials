2c9896334af71dd1bd336fef0518e2d01963b8f2
AleksandrS <ASagalajev92@alex.com>
2020-02-29 15:53:05 +0200

    More practice with functions and overloading.

--- src/KotlinTutorial.kt
+++ src/KotlinTutorial.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NAME_SHADOWING")
+
 fun main() {
 //    val name = "Aleks"  // VAL is same as FINAL in java.
 //    var age = 27
@@ -94,17 +96,161 @@ fun main() {
     homeworkNumbers(10, 10)
     println(double(4))
 
-    println(getSum(2,2,2,3))
+    println(getSum(2, 2, 2, 3))
 
-    val array = intArrayOf(1,2,645,8,6786)
-    println(getSum2(2,4,6,7,8,3,234,*array,234,56,7))           // * - Symbol makes int to intArray . Without * appears ERROR.
+    val array = intArrayOf(1, 2, 645, 8, 6786)
+    println(
+        getSum2(
+            2,
+            4,
+            6,
+            7,
+            8,
+            3,
+            234,
+            *array,
+            234,
+            56,
+            7
+        )
+    )           // * - Symbol makes int to intArray . Without * appears ERROR.
 
-    val array2 = intArrayOf(11,22,64,8,645)
-    println(biggestOneInArray(0,65,3,*array2))
+    val array2 = intArrayOf(11, 22, 64, 8, 645)
+    println(biggestOneInArray(0, 65, 3, *array2))
+
+    val s1: String? =
+        null                                               // If You need null result You can add :Type? to variable
+    val s2: String? = "Summa"
+    val s3: String? = "simvolov v strokah."
+
+    val resultSum: Int = (s1?.length ?: 0) + (s2?.length ?: 0) + (s3?.length
+        ?: 0)  // When You check nullable variable SAFE METHOD is, to use VAR.?METHOD
+    // if variable contents null = returns "null" string or You can put
+    // VAR.?METHOD?:VALUE = if VAR is null returns VALUE
+    println(resultSum)
+
+    val currentTime = 10
+    val isGoodWeather = false
+
+    val timeResult = when {
+        isGoodWeather && currentTime in 8..15 -> "Need to go outside and walk little bit !"
+        !isGoodWeather && currentTime in 8..15 -> "Weather is not good ! Better stay at home and read a book !"
+        currentTime in 0..7 -> "Its night time ! Sleep little hacker !"
+        else -> "This is evening or that time on Earth we don't have !"
+    }
+    println(timeResult)
+
+    val user1 = User()
+    user1.age = 30
+    user1.weight = 200
+    user1.name  = "Fatty Boy"
+
+    println("My name is ${user1.name} and I am ${user1.age} years old. My weight is ${user1.weight} kg! ")
+
+    val mutable: MutableList<Int> = arrayListOf(1, 202, 3, 4, 7, 6, 8, 345)                       // Clean architecture say that we must use MutableList
+    val hundredArray : Array<Int?> = arrayOfNulls<Int?>(301)                                                                                              // for our collections, because with mutable
+                                                                                                // we can change collection type without big changes.
+    var resultMutable = 0
+    for (i2 in mutable) {
+        if ((i2 % 2) == 0) {
+            resultMutable += i2     // If element in array on % = 0 , then sum all same elements in array.
+        }
+        continue                    // else continue
+    }
+    println(resultMutable)
+
+    for ((index,element) in (600 downTo 300).withIndex()) {
+        hundredArray[index] = element
+        if ((element % 5) == 0) {
+            print("${hundredArray[index]} ")
+        }
+        continue
+    }
+    println()
+    println(crop("Corona Virus"))                       // # Virus
+
+    val arrayTest = arrayOf(1, 4, 6, 10, 3, 4) // Manual initialization of array
+    println(arrayTest[2])                               // printing 2 element in array
+    arrayTest[2] = arrayTest[2-1]                       // changing array element with index 2 to value of element with index 1
+    println(arrayTest[2])                               // printing 3 element with index 2
+    println(arrayTest[1])                               // printing 2 element with index 1
+
+    val resultOfArraySort = sortArray(arrayTest)        // Applying the sort method to array
+    for (number in resultOfArraySort) {                       // Reading all sorted elements from sorted array
+        print("$number ")                                         // Printing each element to line with separation " "                # 1 3 4 4 4 10
+    }
+    println()
+
+    printInfo("Bobkovich", "Bill","James")
+    printInfo(name = "Bulbazavr")
+
+    val volume = volumeRectangle(2,4,4)
+    println(volume)
 
 
+
+
+    val arraySorted = readLine()!!.split(" ").map { it.toInt() }.toMutableList()  // Taking input string numbers from console
+                                                                                                            // splitting them with " " element
+                                                                                                            // Mapping, converting String numbers to Int
+    //                                                                                                  Initialization of entered numbers to MutableList()
+    val countMap = arraySorted.count()                         // Counting how much elements we have in map collection
+    println(countMap)
+    val resultOfMutableSort = sort(arraySorted)
+    for (number2 in resultOfMutableSort) {                    // Reading all sorted elements from sorted array
+        print("$number2 ")                                        // Printing each element to line with separation " "
+    }
 
 }
+
+
+    // FUNCTIONS
+
+
+
+fun volumeRectangle(length: Double = 0.0, height: Double = 0.0, width: Double = 0.0):Double = length * height * width // Function to calculate Double type volume
+fun volumeRectangle(length: Int, height: Int = length, width: Int = length):Int = length * height * width             // Same overloaded function to calculate Int type volume
+
+
+fun printInfo(lastName: String = "", name: String = "", patronymic: String = "") {
+    if (patronymic.isNotEmpty()) {
+        println("Last name : $lastName Name : $name Patronymic : ${patronymic.ifBlank { "ERROR" }}")
+                                                                  // .ifBlank checks if string = null or " " return { something }
+    }
+    else println("Last name : $lastName Name : $name")
+}
+
+fun sort(numbers: MutableList<Int>): List<Int> {   // BUBBLE SORT of numbers in MutableList
+    for (i in 1 until numbers.size) {
+        for (j in numbers.size - 1 downTo i) {
+            if (numbers[j] < numbers[j - 1]) {
+                val numberJ = numbers[j]
+                numbers[j] = numbers[j - 1]
+                numbers[j - 1] = numberJ
+            }
+        }
+    }
+    return numbers
+}
+
+fun sort(numbers: Array<Int>) = sort(numbers.toMutableList())           // Overloading function sort for taking Array elements
+fun sort(vararg numbers: Int) = sort(numbers.toMutableList())           // Overloading function sort for taking Int elements
+
+fun sortArray(numbers: Array<Int>): Array<Int> { // BUBBLE SORT of numbers in Array
+    for (i in 1 until numbers.size) {
+        for (j in numbers.size - 1 downTo i) {
+            if (numbers[j] < numbers[j - 1]) {
+                val numberJ = numbers[j]
+                numbers[j] = numbers[j - 1]
+                numbers[j - 1] = numberJ
+            }
+        }
+    }
+    return numbers
+}
+
+
+fun crop(s: String) = s.substring(7,12)
 
 fun greeting(name: String, repeat: Int) {
     for (i in 0 until repeat) {
---
+++ src/User.kt
@@ -0,0 +1,10 @@
+class User {
+    var name = "Aleks"
+    var age = 27
+    var weight = 98
+
+}
+
+fun sumOfFields(age: Int, weight: Int):Int {
+    return age * weight
+}
